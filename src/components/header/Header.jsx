d73e7bf63f2e697ab22a73af136f8e53ac9c6701
Bilostenko <bilostenkod@gmail.com>
2024-02-13 22:23:58 +0200

    update header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,18 @@
 import './header.css'
+import logoIMG from '../../img/icons/logo.svg'
 
 function Header() {
   return (
     <header className='header'>
-      <h1>Header and Nav</h1>
+      <div className="container">
+        <div className="header__row">
+          <div className="header__logo">
+            <img src={logoIMG} alt="logo" />
+            <span>FASHION</span>
+          </div>
+          <div className="header__nav">NAV</div>
+        </div>
+      </div>
     </header>
   );
 }
