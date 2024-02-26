0ec6db5716fe01d9623ac72eb2f0106874f2ab66
Bilostenko <bilostenkod@gmail.com>
2024-02-26 17:46:28 +0200

    add Arriavls-2

--- src/App.js
+++ src/App.js
@@ -3,15 +3,35 @@ import Brands from './components/brands/Brands';
 import Header from './components/header/Header';
 import Payday from './components/payday/Payday';
 import Promo from './components/promo/Promo';
+
+import Hoodies from './img/categories/cat-01.jpg'
+import Coats from './img/categories/cat-02.jpg'
+import Tshirt from './img/categories/cat-03.jpg'
+
+import TwoGirls from './img/images/promo-01.jpg'
+import OneGirl from './img/images/promo-02.jpg'
 function App() {
   return (
     <div className="App">
       <Header />
       <Promo />
       <Brands />
-      <Arrivals />
+      <Arrivals
+        title="NEW ARRIVALS"
+        cardData={[
+          { titleCard: "Hoodies & Sweetshirt", img: Hoodies },
+          { titleCard: "Coats & Parkas", img: Coats },
+          { titleCard: "Tees & T-Shirt", img: Tshirt }
+        ]}
+      />
       <Payday />
-      <Arrivals />
+      <Arrivals
+        title="Young’s Favourite"
+        cardData={[
+          { titleCard: "Trending on instagram", img: TwoGirls },
+          { titleCard: "All Under $40", img: OneGirl },
+        ]}
+      />
     </div>
   );
 }
--- src/components/arrivals/Arrivals.jsx
+++ src/components/arrivals/Arrivals.jsx
@@ -1,21 +1,21 @@
 import './arriavls.css'
-import Hoodies from '../../img/categories/cat-01.jpg'
-import Coats from '../../img/categories/cat-02.jpg'
-import Tshirt from '../../img/categories/cat-03.jpg'
 import Card from './card/Card'
 
-const Arrivals = () => {
+const Arrivals = (props) => {
+
+  const { title, cardData } = props
+
   return (
     <section className="arrivals">
       <div className="section">
         <div className="container">
           <div className="arrivals__header">
-            <h2 className="title2">NEW ARRIVALS</h2>
+            <h2 className="title2">{title}</h2>
           </div>
           <div className="arrivals__content">
-            <Card title={"Hoodies & Sweetshirt"} img={Hoodies}/>
-            <Card title={"Coats & Parkas"} img={Coats}/>
-            <Card title={"Tees & T-Shirt"} img={Tshirt}/>
+            {cardData.map((card, index) => (
+              <Card key={index} title={card.titleCard} img={card.img} />
+            ))}
           </div>
         </div>
       </div>
